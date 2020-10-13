3fc54fa02f7e53e9feab8bc6889f7252295680d8
Aya-Akrabawi <aya.akrabawi.93@hotmail.com>
2020-10-13 10:57:27 +0300

    solve challenge 2

---
+++ challenges/arrayShift/array-shift.js
@@ -0,0 +1,27 @@
+'use strict'
+let array = [4,8,15,23,42]
+let value = 16
+let array1 = [2,4,6,8]
+let value1 = 5
+let newArr = []
+let x
+function insertShiftArray (arr,val){
+    for (let i = 0; i < arr.length/2; i++) {
+      if (arr.length%2===0){
+               x = (arr.length/2)
+      } else{
+       x = (arr.length/2)+0.5
+      }
+       newArr[i] = arr[i]
+       newArr[x] = val
+    }  
+    for (let i = x+1; i <= arr.length; i++) {
+      let y = i-1
+     newArr[i] = arr[y]
+    }
+    return newArr
+}
+// insertShiftArray(array,value)
+// console.log(newArr)
+insertShiftArray(array1,value1)
+console.log(newArr)
