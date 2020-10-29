f18a8bb2b5b27726fb09a56b127070d375f5adf0
Aya-Akrabawi <aya.akrabawi.93@hotmail.com>
2020-10-29 23:45:06 +0300

    resubmit

---
+++ challenges/llZip/ll-zip.js
@@ -0,0 +1,38 @@
+'use strict';
+
+const List = require('../../linkedList/linked-list')
+const Node = require('../../linkedList/node');
+
+
+function zipLists(list1, list2) {
+    let cur1 = list1.head;
+    let cur2 = list2.head;
+    let ll = new List(new Node(cur1.value));
+    cur1 = cur1.next;
+    let count = 0;
+    while (cur1 || cur2) {
+        if (count % 2 === 0 && cur2) {
+            ll.append(new Node(cur2.value))
+            cur2 = cur2.next;
+        }
+        if (count % 2 === 1 && cur1) {
+            ll.append(new Node(cur1.value))
+            cur1 = cur1.next;
+        }
+        count++;
+    }
+    return ll;
+}
+
+const ll1 = new List();
+ll1.insert(4);
+ll1.insert(5);
+ll1.insert(6);
+const ll2 = new List();
+ll2.insert(70);
+ll2.insert(80);
+ll2.insert(90);
+
+console.log('hhhhhhhhhhh',zipLists(ll1,ll2));
+
+module.exports = zipLists;
---
+++ challenges/llZip/ll-zip.test.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const LinkedList = require('../../linkedList/linked-list')
+const Node = require('../../linkedList/node');
+const zipLists = require('./ll-zip')
+
+describe('Linked List', () => {
+it('function works happily', ()=> {
+
+    let listA = new LinkedList();
+    listA.insert('1');
+    listA.insert('2');
+    listA.insert('3');
+    listA.insert('4');
+    listA.insert('5');
+  
+    let listB = new LinkedList();
+    listB.insert('A');
+    listB.insert('B');
+    listB.insert('C');
+    listB.insert('D');
+    listB.insert('E');
+    
+    expect(zipLists(listA, listB).toString()).toEqual('{ 1 } -> { A } -> { 2 } -> { B } -> { 3 } -> { C } -> { 4 } -> { D } -> { 5 } -> { E } ->  NULL');
+  });
+});  
